7f1c40e1b92a31b29b64bb16596e09cb08a80571
Nour El Houda Ben Sliméne <bsnour446@gmail.com>
2026-01-26 22:20:30 +0100

    Create embeddings.py

---
+++ Pathology_Report/embeddings.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import torch
+from transformers import CLIPProcessor, CLIPModel
+from config import CLIP_MODEL_NAME
+
+clip_model = CLIPModel.from_pretrained(CLIP_MODEL_NAME)
+clip_processor = CLIPProcessor.from_pretrained(CLIP_MODEL_NAME)
+clip_model.eval()
+
+def embed_image(image_path):
+    """Calcule l'embedding normalisé d'une image"""
+    image = Image.open(image_path).convert("RGB")
+    inputs = clip_processor(images=image, return_tensors="pt")
+    with torch.no_grad():
+        emb = clip_model.get_image_features(**inputs)
+        emb = emb / emb.norm(dim=-1, keepdim=True)
+    return emb.squeeze().tolist()
