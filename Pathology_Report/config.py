c72db779c2b67b00485229eeb1dc2e9b2114d731
Nour El Houda Ben Sliméne <bsnour446@gmail.com>
2026-01-26 22:35:26 +0100

    Update config.py

--- Pathology_Report/config.py
+++ Pathology_Report/config.py
@@ -1,16 +1,15 @@
-
-import os
-
 # Qdrant
-QDRANT_URL = "https://5f634570-7d75-4753-a450-5a595145d6bf.europe-west3-0.gcp.cloud.qdrant.io"
-QDRANT_API_KEY = "VOTRE_API_KEY_ICI"  # tu peux le remplacer par userdata.get("api-key") si dans Colab
-
+QDRANT_URL = "https://9f7628b3-03d5-4d18-be32-5190084b4cef.us-east4-0.gcp.cloud.qdrant.io"
+QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.5LfFf_168iUPnit2IC1C8xAGm6XNdm0ETZL_nxQ1Cj8"
 COLLECTION_NAME = "PathologyImage"
 
 # Dataset
-DATA_DIR = "/content/synthetic_breast_dataset"
-IMG_DIR = os.path.join(DATA_DIR, "images")
-METADATA_FILE = os.path.join(DATA_DIR, "metadata.jsonl")
+
+DATA_DIR = "/kaggle/input/githubbbb/Pathology_Report/data/breast_dataset"
+METADATA_FILE = DATA_DIR + "/metadata.jsonl"
+CSV_FILE = "/kaggle/input/initinalr/output_clean.csv"
+CSV_COLUMN_NAME = "Patient_ID"
+OUTPUT_METADATA_FILE = "/kaggle/working/metadata_updated.jsonl"
 
 # CLIP
 CLIP_MODEL_NAME = "openai/clip-vit-base-patch32"
